c7b3f95aa74ab4de3f784e112c7c90648de4e85a
st sama <albertezoula98@gmail.com>
2024-08-21 17:48:40 +0000

    conditional rendering in react js

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,8 @@
-import Student from './Student.jsx';
+import UserGreeting from "./UserGreeting";
 function App() {
   return(
     <>
-      <Student name="st Sama" age={27} isStudent={true}/>
-      <Student name="Ezoulson" age={29} isStudent={true}/>
-      <Student name="Micheal" age={30} isStudent={false}/>
-      <Student name="John" age={25} isStudent={false}/>
-      <Student name="Doe" age={26} isStudent={true}/>
-      <Student />
-      <Student age={50} />
-      <Student isStudent={true} />
+      <UserGreeting isLoggedIn={true} username="StSama" />
     </>
   );
 }
