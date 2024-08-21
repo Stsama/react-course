0da49363790c705ea34b38617b7246c579120511
st sama <albertezoula98@gmail.com>
2024-08-21 20:47:20 +0000

    list rendering in REACT JS

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,25 @@
-import UserGreeting from "./UserGreeting";
+import List from "./List";
+
 function App() {
+  const fruits = [  {id:0, name:"mango", calories: 100},
+    {id:1, name:"banana", calories: 65},
+    {id:2, name:"coconut", calories: 32}, 
+    {id:3, name:"pineapple", calories: 102}, 
+    {id:4, name:"orange", calories: 99} 
+  ]
+
+  const vegetables = [
+    {id:5, name:"tomatoes", calories: 50},
+    {id:6, name:"carotes", calories: 25}, 
+    {id:7, name:"corn", calories: 65}, 
+    {id:8, name:"broccoli", calories: 50},
+    {id:9, name:"patatoes", calories: 120}
+  ]
+
   return(
     <>
-      <UserGreeting isLoggedIn={true} username="StSama" />
+      {fruits.length > 0 && <List items={fruits} category="fruits" /> } 
+      {vegetables.length > 0 ? <List items={vegetables} category="vegetables" /> : null}
     </>
   );
 }
